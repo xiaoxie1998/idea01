bf599300e453484460d0cca60d51c43467490ba5
tang <506180102@qq.com>
2019-05-20 16:03:26 +0800

    第一次

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("老黑：Hello World!");
+        System.out.println("args = " + args);
+    }
+
+
+    /**
+     *
+     * @param a
+     * @param b
+     */
+    public static void add(int a,int b){
+        System.out.println(a+b);
+
+    }
+
+
+}
